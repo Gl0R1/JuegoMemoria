3f673bb801dd8d89a5a9da31f9dc2c8423689d31
gloria <gloria17alternativa@gmail.com>
2023-10-04 18:48:05 -0300

    primer commit

---
+++ src/import.js
@@ -0,0 +1,57 @@
+import src1 from './img/personajes/kitty.png'
+import src2 from './img/personajes/choco_cat.png'
+import src3 from './img/personajes/coneja.png'
+import src4 from './img/personajes/sapo.jpg'
+import src5 from './img/personajes/gatita.jpg'
+import src6 from './img/personajes/sanrio.png'
+const images = [
+  {
+    src: src1,
+    player: 'kitty'
+  },
+  {
+    src: src1,
+    player: 'kitty'
+  },
+  {
+    src: src2,
+    player: 'choco cat'
+  },
+  {
+    src: src2,
+    player: 'choco cat'
+  },
+  {
+    src: src3,
+    player: 'coneja'
+  },
+  {
+    src: src3,
+    player: 'coneja'
+  },
+  {
+    src: src4,
+    player: 'sapo'
+  },
+  {
+    src: src4,
+    player: 'sapo'
+  },
+  {
+    src: src5,
+    player: 'gatita'
+  },
+  {
+    src: src5,
+    player: 'gatita'
+  },
+  {
+    src: src6,
+    player: 'sanrio'
+  },
+  {
+    src: src6,
+    player: 'sanrio'
+  },
+];
+export { images }
